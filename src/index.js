ef09cbd4cc51dc22832c605d339fbbb080ba8711
betul <bsenger@aof.anadolu.edu.tr>
2023-03-12 17:10:07 +0300

    feat : css added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <section> 
-      <p>App 1</p>
+    <section className="container" style={ {border:"3px solid black", padding: "1rem" } }> 
+      <p className='red-text'>App 1</p>
     </section>
 );
